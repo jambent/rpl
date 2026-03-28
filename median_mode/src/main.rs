1c3ddd0031b16cbf6323ee168534b13d75d15544
jambent <benn.james.w@gmail.com>
2026-03-28 17:21:06 +0000

    Adds mode to median_mode

--- median_mode/src/main.rs
+++ median_mode/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::collections::HashMap;
 
 
 fn main() {
@@ -18,13 +19,15 @@ fn main() {
         }
         match input_str.trim().parse::<i32>() {
             Ok(n) => int_vector.push(n),
-            Err(e) => println!("Integer not recognised")
+            Err(_e) => println!("Integer not recognised")
         }
     }
     int_vector.sort();
-    println!("{:?}",int_vector);
     let median_result:f32 = median(&int_vector);
-    println!("{:?}",median_result);
+    let mode_result:i32 = mode(&int_vector);
+    println!("The median of the input integers is {:?}"
+                ,median_result);
+    println!("The mode is {:?}", mode_result);
 }
 
 fn median(int_vector: &Vec<i32>) -> f32{
@@ -39,4 +42,22 @@ fn median(int_vector: &Vec<i32>) -> f32{
         return ((int_vector[lower_middle_index] +
             int_vector[upper_middle_index]) / 2)  as f32;
     }
+}
+
+fn mode(int_vector: &Vec<i32>) -> i32{
+    let mut int_counts = HashMap::new();
+    for value in int_vector {
+        let count = int_counts.entry(value).or_insert(0);
+        *count += 1;
+    }
+    let mut mode_value:&i32 = &int_vector[0]; 
+    let mut count_for_mode_value:i32 = int_counts.get(&mode_value).copied().unwrap_or(0);
+    for value in int_vector {
+        let count_for_value:i32 = int_counts.get(value).copied().unwrap_or(0);
+        if count_for_value > count_for_mode_value {
+            mode_value = value;
+            count_for_mode_value = count_for_value;
+        } 
+    }
+    return *mode_value;
 }
